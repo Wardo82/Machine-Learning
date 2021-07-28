8560c37b45eea52130e4b3349c78a61998da9439
Eduardo Flores <>
2021-07-28 20:41:57 +0200

    First commit

---
+++ install_dependencies.sh
@@ -0,0 +1,33 @@
+# Install dependencies
+sudo apt -y install build-essential
+sudo apt -y install unzip
+sudo apt -y install git
+sudo apt -y install cmake
+sudo apt -y install cmake-curses-gui
+sudo apt -y install python
+sudo apt -y install python-pip
+sudo apt -y install libblas-dev
+sudo apt -y install libopenblas-dev
+sudo apt -y install libatlas-base-dev
+sudo apt -y install liblapack-dev
+sudo apt -y install libboost-all-dev
+#sudo apt -y install libopencv-core3.2
+#sudo apt -y install libopencv-imgproc3.2
+#sudo apt -y install libopencv-dev
+#sudo apt -y install libopencv-highgui3.2
+#sudo apt -y install libopencv-highgui-dev
+sudo apt -y install protobuf-compiler
+sudo apt -y install libprotobuf-dev
+sudo apt -y install libhdf5-dev
+sudo apt -y install libjson-c-dev
+sudo apt -y install libx11-dev
+sudo apt -y install openjdk-8-jdk
+sudo apt -y install wget
+sudo apt -y install ninja-build
+
+# Install the necessary linear algebra packages (comment the ones you won't use)
+sudo apt -y install libeigen3-dev
+
+
+# Install python packages
+pip install pyyaml typing
